87e1128702f959441af34076cdd338ea2591b31c
Antônio Roberto Silva <forevertonny@gmail.com>
2016-03-31 19:27:49 -0300

    mail message can handle with liquid templates

--- app/models/mail_message.rb
+++ app/models/mail_message.rb
@@ -3,4 +3,51 @@
 class MailMessage < ActiveRecord::Base
   validates :label, uniqueness: true
   validates :label, :subject, :body, presence: true
+
+  attr_accessor :parsed_subject, :parsed_body
+
+  # handles with template parse for message
+  # attributes should be in format:
+  # attrs = {
+  #   subject_data: {
+  #     'var1' => 'ipsum'
+  #   },
+  #   body_data: {
+  #     'var1' => 'lorem'
+  #   }
+  # }
+  def parse(attrs)
+    parse_subject(attrs.delete(:subject_data))
+    parse_body(attrs.delete(:body_data))
+  end
+
+  # parse subject_template with given attributes
+  # rendered template are stored into parsed_subject
+  def parse_subject(attrs = {})
+    self.parsed_subject = subject_template.render(
+      attrs, strict_variables: true)
+  end
+
+  # parse body_template with given attributes
+  # rendered template are stored into parsed_body
+  def parse_body(attrs = {})
+    self.parsed_body = body_template.render(
+      attrs, strict_variables: true)
+  end
+
+  # subject template over template parser
+  def subject_template(parser = template_parser)
+    @st ||= parser.parse(subject)
+  end
+
+  # body template over template parser
+  def body_template(parser = template_parser)
+    @bt ||= parser.parse(body)
+  end
+
+
+  def template_parser
+    Liquid::Template
+  end
+
 end
--- spec/models/mail_message_spec.rb
+++ spec/models/mail_message_spec.rb
@@ -1,14 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe MailMessage, type: :model do
-  let(:mail_message) { create(:mail_message) }
+  let(:mail_message) do
+    create(:mail_message, {
+        subject: 'welcome {{name}}',
+        body: 'hello {{name}}'
+      })
+  end
+  let(:parse_attrs) do
+    {
+      subject_data: {
+        'name' => 'ipsum'
+      },
+      body_data: {
+        'name' => 'lorem'
+      }
+    }
+  end
 
-  describe "validations" do
-    before { mail_message} 
+  describe 'validations' do
+    before { mail_message }
 
     it { should validate_presence_of(:subject) }
     it { should validate_presence_of(:body) }
     it { should validate_presence_of(:label) }
     it { should validate_uniqueness_of(:label) }
+  end
+
+  describe '.parse' do
+    it do
+      expect(mail_message).to receive(:parse_subject)
+      expect(mail_message).to receive(:parse_body)
+      mail_message.parse(parse_attrs)
+    end
+  end
+
+  describe '.parse_subject' do
+    context 'parse wit all variables' do
+      before do
+        mail_message.parse_subject(parse_attrs.delete(:subject_data))
+      end
+
+      it 'should parsed_subject filled' do
+        expect(mail_message.parsed_subject).to eq('welcome ipsum')
+      end
+
+      it 'should have no errors' do
+        expect(mail_message.subject_template.errors).to eq([])
+      end
+    end
+
+    context 'parse with error' do
+      before do
+        mail_message.parse_subject({})
+      end
+
+      it 'should parsed_subject filled' do
+        expect(mail_message.parsed_subject).to eq('welcome ')
+      end
+
+      it 'should have no errors' do
+        expect(mail_message.subject_template.errors).not_to be_empty
+      end
+    end
+  end
+
+  describe '.parse_body' do
+    context 'parse wit all variables' do
+      before do
+        mail_message.parse_body(parse_attrs.delete(:body_data))
+      end
+
+      it 'should parsed_body filled' do
+        expect(mail_message.parsed_body).to eq('hello lorem')
+      end
+
+      it 'should have no errors' do
+        expect(mail_message.body_template.errors).to eq([])
+      end
+    end
+
+    context 'parse with error' do
+      before do
+        mail_message.parse_body({})
+      end
+
+      it 'should parsed_subject filled' do
+        expect(mail_message.parsed_body).to eq('hello ')
+      end
+
+      it 'should have no errors' do
+        expect(mail_message.body_template.errors).not_to be_empty
+      end
+    end
   end
 end
