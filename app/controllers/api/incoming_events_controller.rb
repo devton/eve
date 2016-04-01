6cbefb23d0b7102150751fe562a519f1122035ca
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-01 13:22:08 -0300

    null_session for api controller

--- app/controllers/api/incoming_events_controller.rb
+++ app/controllers/api/incoming_events_controller.rb
@@ -1,5 +1,6 @@
 # This controller handles with incoming events  via api
 class Api::IncomingEventsController < ApplicationController
+  protect_from_forgery with: :null_session
   respond_to :json
 
   # Handles with creation of events
