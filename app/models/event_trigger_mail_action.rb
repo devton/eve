025d6564079f7f1bf59ac4766a804d6a065d0466
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-02 01:27:35 -0300

    adjust validations

--- app/models/event_trigger_mail_action.rb
+++ app/models/event_trigger_mail_action.rb
@@ -5,8 +5,8 @@ class EventTriggerMailAction < ActiveRecord::Base
   belongs_to :mail_message
 
   validates :event_trigger, :mail_message, :step, presence: true
-  validates :step, uniquness: { scope: %i( event_trigger_id ) }
-  validates :exec_after, length: { minimum: 0 }
+  validates :step, uniqueness: { scope: %i( event_trigger_id ) }
+  validates :exec_after, length: { minimum: 0 }, allow_nil: true
 
   scope :step_order, -> () { order(step: :asc) }
 end
