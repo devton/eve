d0f8c787bcea4b67835972aa13ce15ede667f1dc
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-02 18:08:32 -0300

    indexes

--- db/migrate/20160331230321_create_event_triggers.rb
+++ db/migrate/20160331230321_create_event_triggers.rb
@@ -1,9 +1,9 @@
 class CreateEventTriggers < ActiveRecord::Migration
   def change
     create_table :event_triggers do |t|
-      t.text :trigger_name
+      t.text :trigger_name, index: :unique, null: false
       t.text :description
-      t.boolean :enabled
+      t.boolean :enabled, default: true
 
       t.timestamps null: false
     end
