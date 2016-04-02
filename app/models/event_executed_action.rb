523e5921d3a27e65f93f5beabf2970a3308e48c1
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-02 00:56:03 -0300

    executed_action should validate presence of mail action

--- app/models/event_executed_action.rb
+++ app/models/event_executed_action.rb
@@ -2,5 +2,5 @@ class EventExecutedAction < ActiveRecord::Base
   belongs_to :event
   belongs_to :event_trigger_mail_action
 
-  validates :event, presence: true
+  validates :event, :event_trigger_mail_action, presence: true
 end
--- spec/models/event_executed_action_spec.rb
+++ spec/models/event_executed_action_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe EventExecutedAction, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:event) }
+    it { should validate_presence_of(:event_trigger_mail_action) }
     it { should belong_to(:event) }
     it { should belong_to(:event_trigger_mail_action) }
   end
