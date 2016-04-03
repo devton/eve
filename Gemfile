0101ba2afccd394960df76c21c016042a6f902c2
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-03 13:19:19 -0300

    gridhook

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'kaminari'
 gem 'parallel'
 gem 'jsonpath'
 gem 'unicorn'
+gem 'gridhook'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/gridhook.rb
@@ -0,0 +1,9 @@
+Gridhook.configure do |config|
+  # The path we want to receive events
+  config.event_receive_path = '/sendgrid/event'
+
+  config.event_processor = proc do |event|
+    # event is a Gridhook::Event object
+    # EmailEvent.create! event.attributes
+  end
+end
