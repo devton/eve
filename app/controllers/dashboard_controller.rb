3a87ad5b148e2cb6e51fc88bfaa9db8200aa0d53
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-09 11:05:43 -0300

    adjust semantic front to form basics

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
+  respond_to :html
+
   def index
+    @events = Event.order(created_at: :desc).page(params[:page])
+    respond_with @events
   end
 end
