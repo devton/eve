6a6cffb76383dcf4c134dc187a25f5de75dfad8f
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-14 01:04:33 -0300

    use search on event_trigger_catalog

--- app/models/event_trigger_catalog.rb
+++ app/models/event_trigger_catalog.rb
@@ -1,8 +1,12 @@
 # this is an VIEW to aggregate event_trigger events data
 class EventTriggerCatalog < ActiveRecord::Base
+  include PgSearch
   paginates_per 5
   max_paginates_per 10
   self.table_name = 'event_trigger_catalog'
+  self.primary_key = 'event_trigger_id'
+
+  pg_search_scope :search, against: [:trigger_name], using: [:tsearch, :trigram]
 
   def last_activity
     @last_activity ||= EventTriggerCatalogActivity.find_by_event_trigger_id event_trigger_id
